5e2241d32520c7f5adb2a69fb8a4b05392a32355
IBUNHABIBU <ibunhabib@gmail.com>
2025-06-10 18:04:20 +0300

    Adding nav test

---
+++ spec/system/navbar_spec.rb
@@ -0,0 +1,32 @@
+require 'rails_helper'
+
+RSpec.describe 'Navbar', type: :system do
+  before { visit root_path }
+
+  it 'shows all main navigation links' do
+    within 'nav.navbar' do
+      expect(page).to have_link('Home', href: root_path)
+      expect(page).to have_link('About', href: about_path)
+      expect(page).to have_link('Contact', href: contact_path)
+      expect(page).to have_link('Services', href: services_path)
+      expect(page).to have_link('Projects', href: projects_path)
+      expect(page).to have_link('Blog', href: blog_path)
+    end
+  end
+
+  context 'on mobile', js: true do
+    before { resize_window_to_mobile }
+
+    it 'shows mobile menu toggle' do
+      expect(page).to have_css('.navbar__mobile-toggle')
+    end
+
+    it 'toggles menu when clicked' do
+      find('.navbar__mobile-toggle').click
+      expect(page).to have_css('.navbar__menu--open')
+
+      find('.navbar__mobile-toggle').click
+      expect(page).not_to have_css('.navbar__menu--open')
+    end
+  end
+end
