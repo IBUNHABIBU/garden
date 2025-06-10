52e4d76be30beb58ad6efc42d514904bb8f86445
IBUNHABIBU <ibunhabib@gmail.com>
2025-06-10 18:39:59 +0300

    Add home controller

--- app/javascript/controllers/navbar_controller.js
+++ app/javascript/controllers/navbar_controller.js
@@ -1,7 +1,28 @@
 import { Controller } from "@hotwired/stimulus"
 
-// Connects to data-controller="navbar"
 export default class extends Controller {
+  static targets = ["menu", "toggle"]
+
+  toggle() {
+    this.element.dataset.expanded = 
+      this.element.dataset.expanded !== 'true';
+    
+    document.body.style.overflow = 
+      this.element.dataset.expanded === 'true' ? 'hidden' : '';
+  }
+
+  closeOnLargeScreens() {
+    if (window.innerWidth >= 768) {
+      this.element.dataset.expanded = false;
+      document.body.style.overflow = '';
+    }
+  }
+
   connect() {
+    window.addEventListener('resize', this.closeOnLargeScreens.bind(this));
+  }
+
+  disconnect() {
+    window.removeEventListener('resize', this.closeOnLargeScreens);
   }
 }
