22433b1a9f85db26412d8e6c8ad7d7b470568f23
IBUNHABIBU <ibunhabib@gmail.com>
2025-06-10 14:53:38 +0300

    Add nav helper

--- app/helpers/navigation_helper.rb
+++ app/helpers/navigation_helper.rb
@@ -1,3 +1,12 @@
 module NavigationHelper
-    
+    def nav_links
+        [
+            { name: 'Home', path: root_path },
+            { name: 'About', path: about_path },
+            { name: 'Services', path: services_path },
+            { name: 'Projects', path: projects_path },
+            { name: 'Blog', path: blog_path },
+            { name: 'Contact', path: contact_path }
+        ]
+    end 
 end
