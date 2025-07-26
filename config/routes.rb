cbe309f8daa9c9172e7ae1e050b43c4fee61dd88
IBUNHABIBU <ibunhabib@gmail.com>
2025-07-26 22:22:01 +0300

    Add about page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get "about/index"
   get 'about', to: 'about#index', as: :about
   get 'contact', to: 'contact#index', as: :contact
   get 'services', to: 'services#index', as: :services
