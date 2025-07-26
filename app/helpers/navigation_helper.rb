8451d9fb84f15cb934ebd570d738548526deba09
IBUNHABIBU <ibunhabib@gmail.com>
2025-07-26 22:26:18 +0300

    Add about page

--- app/helpers/navigation_helper.rb
+++ app/helpers/navigation_helper.rb
@@ -2,7 +2,7 @@ module NavigationHelper
     def nav_links
         [
             { name: 'Home', path: root_path },
-            { name: 'About', path: root_path },
+            { name: 'About', path: about_path },
             { name: 'Services', path: root_path },
             { name: 'Projects', path: projects_path },
             { name: 'Blog', path: blog_path },
