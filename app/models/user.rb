81cead030b3256adb341160b94f61a5bf2c105c7
IBUNHABIBU <ibunhabib@gmail.com>
2025-07-28 13:24:06 +0300

    Add auth

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+         :confirmable  # Add if email confirmation is needed
+
+  validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP }
+
+  def admin?
+    admin || super_admin? # Add super_admin if needed
+  end
 end
