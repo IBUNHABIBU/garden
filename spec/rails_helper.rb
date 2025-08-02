9596ae744321365853c664e4973f0a82793aba95
IBUNHABIBU <ibunhabib@gmail.com>
2025-08-02 22:54:18 +0300

    Add webdriver

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -4,7 +4,7 @@ ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 
 require 'capybara/rails'
-
+require 'webdrivers'
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 # Uncomment the line below in case you have `--require rails_helper` in the `.rspec` file
