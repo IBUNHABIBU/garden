af986e7f0ee76c173f0cc1407792ba0a69bd151c
IBUNHABIBU <ibunhabib@gmail.com>
2025-08-05 07:52:23 +0300

    Fix errors

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -3,9 +3,8 @@ require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 
-require 'capybara/rails'
-
-require 'selenium/webdriver'
+require 'capybara/rspec'
+require 'selenium-webdriver'
 
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
