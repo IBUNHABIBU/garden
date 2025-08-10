627bd8584e89aba78fc603ad7903e1b4f6869bcb
IBUNHABIBU <ibunhabib@gmail.com>
2025-08-10 19:55:29 +0300

    Fix tests

--- spec/support/windor_helpers.rb
+++ spec/support/windor_helpers.rb
@@ -1,3 +1,10 @@
 module WindowHelpers
-     
+    def resize_window_to_mobile
+        page.driver.browser.manage.window.resize(375, 812)
+    end 
+
+    def resize_window_default
+    # Typical desktop resolution
+       page.driver.browser.manage.window.resize_to(1280, 800)
+    end
 end
