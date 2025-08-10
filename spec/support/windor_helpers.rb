63de2d38662d8286d3d37312a634ec7de489dca0
IBUNHABIBU <ibunhabib@gmail.com>
2025-08-10 20:00:52 +0300

    Fix tests

--- spec/support/windor_helpers.rb
+++ spec/support/windor_helpers.rb
@@ -1,6 +1,6 @@
 module WindowHelpers
     def resize_window_to_mobile
-        page.driver.browser.manage.window.resize(375, 812)
+        page.driver.browser.manage.window.resize_to(375, 812)
     end 
 
     def resize_window_default
