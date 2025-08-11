60f35db3ee61c44d0b26b0bef844f92d4d0320f3
IBUNHABIBU <ibunhabib@gmail.com>
2025-08-11 20:56:44 +0300

    Fix tests

--- spec/system/home_spec.rb
+++ spec/system/home_spec.rb
@@ -1,9 +1,10 @@
 require 'rails_helper'
 
-RSpec.describe 'Home', type: :system do
+RSpec.describe 'Home page', type: :system do
   before { driven_by(:selenium_chrome_headless) }
   it 'can visit the home page' do
     visit root_path
+    expect(page).to have_selector('h1', text: 'Welcome') # adjust text to match your home page
     expect(page).to have_content('Welcome')  
   end
 end
