6745cbf4aa2d75a4b72321151508e3b549643b9b
IBUNHABIBU <ibunhabib@gmail.com>
2025-08-12 02:02:03 +0300

    Fix tests

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,8 +16,8 @@ module.exports = {
           200: '#bae6fd',
           300: '#7dd3fc',
           400: '#38bdf8',
-          500: '#0ea5e9', 
-          600: '#0284c7', // Original - consider changing to 500 or 400
+          500: '#0ea5e9', // Base primary
+          600: '#0284c7',
           700: '#0369a1',
           800: '#075985',
           900: '#0c4a6e',
@@ -40,10 +40,6 @@ module.exports = {
         md: '8px',
         lg: '12px',
         xl: '20px',
-      },
-        boxShadow: {
-        btn: '0 1px 3px 0 rgb(0 0 0 / 0.1), 0 1px 2px -1px rgb(0 0 0 / 0.1)',
-        'btn-hover': '0 4px 6px -1px rgb(0 0 0 / 0.1), 0 2px 4px -2px rgb(0 0 0 / 0.1)'
       }
     },
   },
