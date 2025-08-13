dc827b6eb357b9a4ebe4d3a17b247756169970ab
IBUNHABIBU <ibunhabib@gmail.com>
2025-08-13 22:26:55 +0300

    Add admin dashboard

--- app/controllers/concerns/authenticable.rb
+++ app/controllers/concerns/authenticable.rb
@@ -1,21 +1,21 @@
-module Authenticable
-  extend ActiveSupport::Concern
+# module Authenticable
+#   extend ActiveSupport::Concern
 
-  included do
-    before_action :authenticate_user!
-    before_action :authorize_admin, if: :admin_controller?
+#   included do
+#     before_action :authenticate_user!
+#     before_action :authorize_admin, if: :admin_controller?
 
-    rescue_from CanCan::AccessDenied do |exception|
-      redirect_to root_path, alert: exception.message
-    end
-  end
+#     rescue_from CanCan::AccessDenied do |exception|
+#       redirect_to root_path, alert: exception.message
+#     end
+#   end
 
-  def authorize_admin
-    return if current_user.admin?
-    redirect_to root_path, alert: "Admin access required"
-  end
+#   def authorize_admin
+#     return if current_user.admin?
+#     redirect_to root_path, alert: "Admin access required"
+#   end
 
-  def admin_controller?
-    controller_path.start_with?('admin/')
-  end
-end
+#   def admin_controller?
+#     controller_path.start_with?('admin/')
+#   end
+# end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_07_28_102054) do
+ActiveRecord::Schema[8.0].define(version: 2025_08_13_190518) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
+
+  create_table "projects", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.string "featured_image"
+    t.boolean "published"
+    t.integer "position"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
