ff397387d6a3961d0d45cff553bc5943f7e1956a
IBUNHABIBU <ibunhabib@gmail.com>
2025-08-15 21:49:26 +0300

    Add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,45 +1,18 @@
 Rails.application.routes.draw do
-  get "contacts/new"
-  get "contacts/create"
-  get "bookings/new"
-  get "bookings/create"
-  get "testimonials/index"
-  get "services/index"
-  get "pages/show"
-  namespace :admin do
-    get "dashboard", to: "dashboard#index"
+  devise_for :users
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
 
-    resources :projects do 
-      patch :toggle_publish, on: :member
-      put :sort, on: :collection
-    end
-  end
-  
-  devise_for :users, path: '', path_names: {
-    sign_in:  'adm_login',      # login
-    sign_up:  'super_register'  # signup
-  }
+  root to: 'pages#show', id: 'home'
 
-  
+  resources :services, only: [:index]
   resources :testimonials, only: [:index]
   resources :bookings, only: [:new, :create]
-  
-  get "about/index"
-  get 'about', to: 'about#index', as: :about
-  get 'contact', to: 'contact#index', as: :contact
-  get 'services', to: 'services#index', as: :services
-  get 'projects', to: 'projects#index', as: :projects
-  get 'blog', to: 'blog#index', as: :blog
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  resources :contacts, only: [:new, :create]
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
+  get '/about', to: 'pages#show', id: 'about'
+  get '/contact', to: 'contacts#new'
 
-  # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
-  # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
-  # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
-
-  # Defines the root path route ("/")
-  root "home#index"
+  # Catch-all for pages
+  get '/:id', to: 'pages#show', as: :page
 end
