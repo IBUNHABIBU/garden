ffc266d26040bd928261b06245071db8ed16588e
IBUNHABIBU <ibunhabib@gmail.com>
2025-08-15 17:54:38 +0300

    Add booking controller

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,7 +1,21 @@
 class BookingsController < ApplicationController
-  def new
+   def new
+    @booking = Booking.new
   end
 
   def create
+    @booking = Booking.new(booking_params)
+    
+    if @booking.save
+      redirect_to root_path, notice: "Thank you for your booking request! We'll contact you soon."
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+  
+  def booking_params
+    params.require(:booking).permit(:name, :email, :phone, :tour_type, :start_date, :end_date, :special_requests)
   end
 end
