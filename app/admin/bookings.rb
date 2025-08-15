62193b5792f719ccf2da07544b4f3d0950f67f40
IBUNHABIBU <ibunhabib@gmail.com>
2025-08-15 21:51:50 +0300

    Add admin

---
+++ app/admin/bookings.rb
@@ -0,0 +1,18 @@
+ActiveAdmin.register Booking do
+
+  # See permitted parameters documentation:
+  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+  #
+  # Uncomment all parameters which should be permitted for assignment
+  #
+  # permit_params :name, :email, :phone, :tour_type, :start_date, :end_date, :special_requests
+  #
+  # or
+  #
+  # permit_params do
+  #   permitted = [:name, :email, :phone, :tour_type, :start_date, :end_date, :special_requests]
+  #   permitted << :other if params[:action] == 'create' && current_user.admin?
+  #   permitted
+  # end
+  
+end
