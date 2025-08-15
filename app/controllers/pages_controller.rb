bdf1e108fd639757cd752b704c2f4ab404319e92
IBUNHABIBU <ibunhabib@gmail.com>
2025-08-15 17:53:12 +0300

    Add pages controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,9 @@
 class PagesController < ApplicationController
   def show
+    @page = Page.find_by!(slug: params[:id])
+    @services = Service.limit(3)
+    @testimonials = Testimonial.limit(3)
+  rescue ActiveRecord::RecordNotFound
+    redirect_to root_path, alert: "Page not found"
   end
 end
