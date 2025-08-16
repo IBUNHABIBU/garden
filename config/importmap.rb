a8512b452b8ce0799512d17d92c0e46697bcb393
IBUNHABIBU <ibunhabib@gmail.com>
2025-08-16 16:29:12 +0300

    Register admin

--- config/importmap.rb
+++ config/importmap.rb
@@ -6,7 +6,5 @@ pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
 
-# Add Swiper
-pin "swiper", to: "https://cdn.jsdelivr.net/npm/swiper@9.0.0/swiper-bundle.min.js"
-pin "swiper/css", to: "https://cdn.jsdelivr.net/npm/swiper@9.0.0/swiper-bundle.min.css"
-
+# Add Swiper# config/importmap.rb
+pin "swiper", to: "https://cdn.jsdelivr.net/npm/swiper@9.0.0/swiper-bundle.esm.min.js"
