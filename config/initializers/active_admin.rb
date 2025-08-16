0aaacc17f5fae83ed6cf010fd412cbb2163f335c
IBUNHABIBU <ibunhabib@gmail.com>
2025-08-16 18:08:44 +0300

    Add active admin assets

--- config/initializers/active_admin.rb
+++ config/initializers/active_admin.rb
@@ -356,4 +356,8 @@ ActiveAdmin.setup do |config|
   config.logout_link_path = :destroy_admin_user_session_path
   config.batch_actions = true
   config.localize_format = :long
+
+  config.use_webpacker = false
+  config.stylesheets = false
+  config.javascripts = false
 end
