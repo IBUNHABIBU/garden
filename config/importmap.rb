cc8ac02385b479929325e9387b5067abc60504af
IBUNHABIBU <ibunhabib@gmail.com>
2025-08-16 11:37:36 +0300

    Add swipper js

--- config/importmap.rb
+++ config/importmap.rb
@@ -5,3 +5,8 @@ pin "@hotwired/turbo-rails", to: "turbo.min.js"
 pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
+
+# Add Swiper
+pin "swiper", to: "https://cdn.jsdelivr.net/npm/swiper@9.0.0/swiper-bundle.min.js"
+pin "swiper/css", to: "https://cdn.jsdelivr.net/npm/swiper@9.0.0/swiper-bundle.min.css"
+
