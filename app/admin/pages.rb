4d6adf8031eafbf2092e6d830bb5b16d1c1c5733
IBUNHABIBU <ibunhabib@gmail.com>
2025-08-16 06:35:27 +0300

    Register admin

--- app/admin/pages.rb
+++ app/admin/pages.rb
@@ -14,5 +14,26 @@ ActiveAdmin.register Page do
   #   permitted << :other if params[:action] == 'create' && current_user.admin?
   #   permitted
   # end
+
+    permit_params :title, :slug, :content, :meta_title, :meta_description
+
+  form do |f|
+    f.inputs 'Page Details' do
+      f.input :title
+      f.input :slug
+      f.input :content, as: :quill_editor
+      f.input :meta_title
+      f.input :meta_description
+    end
+    f.actions
+  end
+
+  index do
+    selectable_column
+    column :title
+    column :slug
+    column :created_at
+    actions
+  end
   
 end
