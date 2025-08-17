323f355cfa4623e64826886600d159d8f2c8e9fa
IBUNHABIBU <ibunhabib@gmail.com>
2025-08-17 07:17:53 +0300

    Add styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,9 @@ module.exports = {
   theme: {
     extend: {
       extend: {
+      animation: {
+        'safari-pulse': 'pulse 3s cubic-bezier(0.4, 0, 0.6, 1) infinite',
+        },
       colors: {
         primary: {
           DEFAULT: '#1E5631',
