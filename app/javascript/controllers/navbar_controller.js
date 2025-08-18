5b377b5203b0cd67a20812741f34ca5dbc06ad11
IBUNHABIBU <ibunhabib@gmail.com>
2025-08-18 10:50:53 +0300

    Add nav controller

--- app/javascript/controllers/navbar_controller.js
+++ app/javascript/controllers/navbar_controller.js
@@ -1,13 +1,23 @@
 import { Controller } from "@hotwired/stimulus"
+import { enter, leave } from "el-transition"
 
 export default class extends Controller {
   static targets = ["menu"]
-
-  toggle() {
-    this.menuTarget.classList.toggle("hidden")
+  
+  toggleMenu() {
+    if (this.menuTarget.classList.contains('hidden')) {
+      enter(this.menuTarget)
+      this.menuTarget.classList.remove('hidden')
+    } else {
+      leave(this.menuTarget).then(() => {
+        this.menuTarget.classList.add('hidden')
+      })
+    }
   }
-
-  close() {
-    this.menuTarget.classList.add("hidden")
+  
+  closeMenu() {
+    leave(this.menuTarget).then(() => {
+      this.menuTarget.classList.add('hidden')
+    })
   }
 }
