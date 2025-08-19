ac7822e7b206011cfa653de2a3f40c5b1793ba69
IBUNHABIBU <ibunhabib@gmail.com>
2025-08-19 17:04:43 +0300

    add trekking

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_08_19_054216) do
+ActiveRecord::Schema[8.0].define(version: 2025_08_19_140312) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -87,6 +87,16 @@ ActiveRecord::Schema[8.0].define(version: 2025_08_19_054216) do
     t.boolean "featured"
     t.text "highlights"
     t.text "includes"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "trekkings", force: :cascade do |t|
+    t.string "title"
+    t.integer "duration"
+    t.string "difficulty"
+    t.string "overview_title"
+    t.text "overview_description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
