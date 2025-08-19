e5ade6e23fade0af6f92f968cc89d9daaf88a780
IBUNHABIBU <ibunhabib@gmail.com>
2025-08-19 08:08:27 +0300

    Add testimonial model

--- app/models/testimonial.rb
+++ app/models/testimonial.rb
@@ -1,2 +1,14 @@
 class Testimonial < ApplicationRecord
+      has_one_attached :avatar
+
+  validates :author_name, presence: true
+  validates :content, presence: true, length: { minimum: 20 }
+  validates :rating, presence: true, 
+                     numericality: { only_integer: true, 
+                                     greater_than_or_equal_to: 1, 
+                                     less_than_or_equal_to: 5 }
+
+  def star_rating
+    '★' * rating + '☆' * (5 - rating)
+  end
 end
