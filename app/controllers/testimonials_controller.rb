7b571346bbbc3a5cd5f6618191bdb9d52879bded
IBUNHABIBU <ibunhabib@gmail.com>
2025-08-19 08:13:11 +0300

    Add testimonial form

--- app/controllers/testimonials_controller.rb
+++ app/controllers/testimonials_controller.rb
@@ -1,70 +1,49 @@
 class TestimonialsController < ApplicationController
-  before_action :set_testimonial, only: %i[ show edit update destroy ]
+  before_action :set_testimonial, only: [:show, :edit, :update, :destroy]
 
-  # GET /testimonials or /testimonials.json
   def index
-    @testimonials = Testimonial.all
+    @testimonials = Testimonial.all.order(created_at: :desc)
   end
 
-  # GET /testimonials/1 or /testimonials/1.json
   def show
   end
 
-  # GET /testimonials/new
   def new
     @testimonial = Testimonial.new
   end
 
-  # GET /testimonials/1/edit
   def edit
   end
 
-  # POST /testimonials or /testimonials.json
   def create
     @testimonial = Testimonial.new(testimonial_params)
 
-    respond_to do |format|
-      if @testimonial.save
-        format.html { redirect_to @testimonial, notice: "Testimonial was successfully created." }
-        format.json { render :show, status: :created, location: @testimonial }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @testimonial.errors, status: :unprocessable_entity }
-      end
+    if @testimonial.save
+      redirect_to @testimonial, notice: 'Testimonial was successfully created.'
+    else
+      render :new
     end
   end
 
-  # PATCH/PUT /testimonials/1 or /testimonials/1.json
   def update
-    respond_to do |format|
-      if @testimonial.update(testimonial_params)
-        format.html { redirect_to @testimonial, notice: "Testimonial was successfully updated.", status: :see_other }
-        format.json { render :show, status: :ok, location: @testimonial }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @testimonial.errors, status: :unprocessable_entity }
-      end
+    if @testimonial.update(testimonial_params)
+      redirect_to @testimonial, notice: 'Testimonial was successfully updated.'
+    else
+      render :edit
     end
   end
 
-  # DELETE /testimonials/1 or /testimonials/1.json
   def destroy
-    @testimonial.destroy!
-
-    respond_to do |format|
-      format.html { redirect_to testimonials_path, notice: "Testimonial was successfully destroyed.", status: :see_other }
-      format.json { head :no_content }
-    end
+    @testimonial.destroy
+    redirect_to testimonials_url, notice: 'Testimonial was successfully destroyed.'
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_testimonial
-      @testimonial = Testimonial.find(params.expect(:id))
+      @testimonial = Testimonial.find(params[:id])
     end
 
-    # Only allow a list of trusted parameters through.
     def testimonial_params
-      params.expect(testimonial: [ :author_name, :author_title, :content, :rating ])
+      params.require(:testimonial).permit(:author_name, :author_title, :content, :rating, :avatar)
     end
 end
