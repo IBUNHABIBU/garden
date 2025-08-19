2a06108df7ce1a6dc90211f5afb689aa735a756d
IBUNHABIBU <ibunhabib@gmail.com>
2025-08-19 08:13:50 +0300

    Add testimonial form

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,5 +3,6 @@ class HomeController < ApplicationController
     @hero = Hero.first
     @featured_tours = TravelTour.featured.limit(3)
     @popular_destinations = Destination.featured.limit(3) # Add this line
+    @testimonials = Testimonial.order(rating: :desc).limit(3) # Add this line
   end
 end
