0eedf62a575a15cbce35b40cb09ea290a3db6792
IBUNHABIBU <ibunhabib@gmail.com>
2025-08-21 15:50:21 +0300

    Add domain

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_08_19_051112) do
+ActiveRecord::Schema[8.0].define(version: 2025_08_19_140912) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
+
+  create_table "action_text_rich_texts", force: :cascade do |t|
+    t.string "name", null: false
+    t.text "body"
+    t.string "record_type", null: false
+    t.bigint "record_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["record_type", "record_id", "name"], name: "index_action_text_rich_texts_uniqueness", unique: true
+  end
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -77,6 +87,16 @@ ActiveRecord::Schema[8.0].define(version: 2025_08_19_051112) do
     t.boolean "featured"
     t.text "highlights"
     t.text "includes"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "trekkings", force: :cascade do |t|
+    t.string "title"
+    t.integer "duration"
+    t.string "difficulty"
+    t.string "overview_title"
+    t.text "overview_description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
