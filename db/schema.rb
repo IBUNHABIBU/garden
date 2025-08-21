19413c3b59a2baf9429f57732ced9b1b7f0b7b9f
IBUNHABIBU <ibunhabib@gmail.com>
2025-08-21 16:26:35 +0300

    Add booking index

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_08_19_140912) do
+ActiveRecord::Schema[8.0].define(version: 2025_08_21_130633) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -50,6 +50,20 @@ ActiveRecord::Schema[8.0].define(version: 2025_08_19_140912) do
     t.bigint "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
+  end
+
+  create_table "bookings", force: :cascade do |t|
+    t.string "full_name"
+    t.string "email"
+    t.string "phone"
+    t.string "trek_name"
+    t.date "start_date"
+    t.date "end_date"
+    t.integer "number_of_people"
+    t.text "special_requests"
+    t.string "status"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "destinations", force: :cascade do |t|
