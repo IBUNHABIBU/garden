f19f583a1299227e74a3b24da84a1e35fee7946d
IBUNHABIBU <ibunhabib@gmail.com>
2025-08-23 01:43:36 +0300

    Add devise

--- app/javascript/controllers/mobile_menu_controller.js
+++ app/javascript/controllers/mobile_menu_controller.js
@@ -1,9 +1,41 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = ["menu"]
+  static targets = ["menu", "openIcon", "closeIcon"]
+
+  connect() {
+    // Initialize the menu state
+    this.isOpen = false
+    this.updateIcons()
+  }
 
   toggle() {
-    this.menuTarget.classList.toggle("hidden")
+    this.isOpen = !this.isOpen
+    this.updateMenu()
+    this.updateIcons()
+  }
+
+  close() {
+    this.isOpen = false
+    this.updateMenu()
+    this.updateIcons()
+  }
+
+  updateMenu() {
+    if (this.isOpen) {
+      this.menuTarget.classList.remove('hidden')
+    } else {
+      this.menuTarget.classList.add('hidden')
+    }
+  }
+
+  updateIcons() {
+    if (this.isOpen) {
+      this.openIconTarget.classList.add('hidden')
+      this.closeIconTarget.classList.remove('hidden')
+    } else {
+      this.openIconTarget.classList.remove('hidden')
+      this.closeIconTarget.classList.add('hidden')
+    }
   }
 }
