1b88b26323156056195e197a45b71f3c61f37e2b
IBUNHABIBU <ibunhabib@gmail.com>
2025-08-23 01:45:09 +0300

    Add devise

--- app/javascript/controllers/mobile_menu_controller.js
+++ app/javascript/controllers/mobile_menu_controller.js
@@ -1,31 +1,39 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = ["menu", "openIcon", "closeIcon"]
+  static targets = ["menu", "openIcon", "closeIcon", "overlay"]
 
   connect() {
-    // Initialize the menu state
     this.isOpen = false
     this.updateIcons()
+    this.setupEventListeners()
+  }
+
+  disconnect() {
+    this.removeEventListeners()
   }
 
   toggle() {
     this.isOpen = !this.isOpen
     this.updateMenu()
     this.updateIcons()
+    this.updateOverlay()
   }
 
   close() {
     this.isOpen = false
     this.updateMenu()
     this.updateIcons()
+    this.updateOverlay()
   }
 
   updateMenu() {
     if (this.isOpen) {
       this.menuTarget.classList.remove('hidden')
+      document.body.classList.add('overflow-hidden')
     } else {
       this.menuTarget.classList.add('hidden')
+      document.body.classList.remove('overflow-hidden')
     }
   }
 
@@ -36,6 +44,29 @@ export default class extends Controller {
     } else {
       this.openIconTarget.classList.remove('hidden')
       this.closeIconTarget.classList.add('hidden')
+    }
+  }
+
+  updateOverlay() {
+    if (this.isOpen) {
+      this.overlayTarget.classList.remove('hidden')
+    } else {
+      this.overlayTarget.classList.add('hidden')
+    }
+  }
+
+  setupEventListeners() {
+    this.boundHandleEscape = this.handleEscape.bind(this)
+    document.addEventListener('keydown', this.boundHandleEscape)
+  }
+
+  removeEventListeners() {
+    document.removeEventListener('keydown', this.boundHandleEscape)
+  }
+
+  handleEscape(event) {
+    if (event.key === 'Escape' && this.isOpen) {
+      this.close()
     }
   }
 }
