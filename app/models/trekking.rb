0c8508df4c946f187a58dc64e1903049c547191b
IBUNHABIBU <ibunhabib@gmail.com>
2025-08-22 11:28:49 +0300

    Add action text

--- app/models/trekking.rb
+++ app/models/trekking.rb
@@ -6,5 +6,10 @@ class Trekking < ApplicationRecord
   validates :difficulty, presence: true, inclusion: { in: %w[easy medium challenging] }
   validates :overview_title, presence: true
   validates :overview_description, presence: true
-  validates :content, presence: true
+
+  def content_must_have_text
+    if content.blank? || content.body.to_plain_text.strip.blank?
+      errors.add(:content, "can't be blank")
+    end
+  end
 end
