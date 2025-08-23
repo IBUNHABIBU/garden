7c60445436c8c5a6908028a7a1d8bb456f187ebc
IBUNHABIBU <ibunhabib@gmail.com>
2025-08-23 20:12:59 +0300

    Add users controller

--- config/routes.rb
+++ config/routes.rb
@@ -30,6 +30,12 @@ Rails.application.routes.draw do
   # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
 
+  # config/routes.rb
+  devise_for :users, path: '', path_names: {
+    sign_up: 'adminregister',
+  }
+
+
   # Defines the root path route ("/")
   root "home#index"
 end
