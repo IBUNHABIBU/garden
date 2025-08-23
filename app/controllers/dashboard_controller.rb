c8d596487fb702da5f4dedaf218da3da9aaed8c2
IBUNHABIBU <ibunhabib@gmail.com>
2025-08-23 20:31:05 +0300

    Add users controller

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,4 +1,16 @@
 class DashboardController < ApplicationController
+  before_action :authenticate_user!
+  before_action :require_admin_access
+
   def index
+    # You can add any dashboard statistics here later
+  end
+
+  private
+
+  def require_admin_access
+    unless current_user.admin? || current_user.super_admin?
+      redirect_to root_path, alert: "You are not authorized to access the dashboard."
+    end
   end
 end
