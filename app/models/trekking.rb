03e371e70d99fd6a6957719149645fdef01b2fcf
IBUNHABIBU <ibunhabib@gmail.com>
2025-08-25 22:41:29 +0300

    Update the contact page

--- app/models/trekking.rb
+++ app/models/trekking.rb
@@ -5,7 +5,7 @@ class Trekking < ApplicationRecord
   
   validates :title, presence: true
   validates :duration, presence: true, numericality: { only_integer: true, greater_than: 0 }
-  validates :difficulty, presence: true, inclusion: { in: %w[easy medium challenging] }
+  validates :difficulty, presence: true, inclusion: { in: %w[easy moderate challenging] }
   validates :overview_title, presence: true
   validates :overview_description, presence: true
   validates :content, presence: true
