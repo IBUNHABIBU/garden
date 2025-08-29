6aae0ce7d26bd0befcf968bd58955356f59dcb52
IBUNHABIBU <ibunhabib@gmail.com>
2025-08-29 23:12:43 +0300

    Add model tests

--- spec/models/travel_tour_spec.rb
+++ spec/models/travel_tour_spec.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe TravelTour, type: :model do
+  subject { build(:travel_tour) } 
   describe 'validations' do
     it { should validate_presence_of(:name) }
     it { should validate_presence_of(:category) }
