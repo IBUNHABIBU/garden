1930ee68fb4b848d548bf284c76a3ac652e34549
IBUNHABIBU <ibunhabib@gmail.com>
2025-08-30 17:07:10 +0300

    Add seo meta descriptions

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -85,11 +85,11 @@ module ApplicationHelper
   end
 
   def full_title(page_title = '')
-  base_title = "Kilimarvelous Tour Company"
-  if page_title.empty?
-    base_title
-  else
-    "#{page_title} | #{base_title}"
-  end
+    base_title = "Kilimarvelous Tour Company"
+    if page_title.empty?
+      base_title
+    else
+      "#{page_title} | #{base_title}"
+    end
   end
 end
