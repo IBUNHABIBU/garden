043830b2eb5e0287767aa8b733aa15a986c2e9fd
IBUNHABIBU <ibunhabib@gmail.com>
2025-08-30 16:49:33 +0300

    Add seo meta descriptions

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_08_29_040809) do
+ActiveRecord::Schema[8.0].define(version: 2025_08_30_130910) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -73,6 +73,8 @@ ActiveRecord::Schema[8.0].define(version: 2025_08_29_040809) do
     t.boolean "featured"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "slug"
+    t.index ["slug"], name: "index_destinations_on_slug", unique: true
   end
 
   create_table "friendly_id_slugs", force: :cascade do |t|
