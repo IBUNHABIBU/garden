86d041b20eb66ec10bbe5b746247d9740619cd2f
IBUNHABIBU <ibunhabib@gmail.com>
2025-08-30 14:56:07 +0300

    Add model tests

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,10 +1,11 @@
 # spec/factories/users.rb
 FactoryBot.define do
   factory :user do
-    name { "John Doe" }
-    email { "johndoe@gmail.com" }
-    password { "password123" }
-    password_confirmation { "password123" }
+   first_name { Faker::Name.first_name }
+    last_name  { Faker::Name.last_name }
+    email      { Faker::Internet.unique.email }
+    password   { "password123" }
+    role       { "user" }
 
     trait :admin do
       role { :admin }
