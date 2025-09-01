e5703346878d82f407220d7ccb3695568d610e91
IBUNHABIBU <ibunhabib@gmail.com>
2025-09-02 02:35:35 +0300

    Add confirmation

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,6 +2,23 @@ require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
+
+  # config/environments/production.rb
+config.action_mailer.default_url_options = { host: 'kilimarvellous.com' }
+config.action_mailer.delivery_method = :smtp
+config.action_mailer.perform_deliveries = true
+config.action_mailer.raise_delivery_errors = true
+
+# SMTP settings (using Gmail as example)
+config.action_mailer.smtp_settings = {
+  address: 'smtp.gmail.com',
+  port: 587,
+  domain: 'kilimarvellous.com',
+  user_name: ENV['Salum Habibu'],
+  password: ENV['Greaterthan100'],
+  authentication: 'plain',
+  enable_starttls_auto: true
+}
   
   # Code is not reloaded between requests.
   config.enable_reloading = false
