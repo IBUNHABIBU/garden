554dfb942939bdd5225eb405e2cb3aa3ab8742cb
IBUNHABIBU <ibunhabib@gmail.com>
2025-09-02 02:49:29 +0300

    Add latter opener gem

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,12 @@ class ApplicationController < ActionController::Base
 
   protected
 
+  def check_confirmation
+    if user_signed_in? && !current_user.confirmed?
+      redirect_to new_user_confirmation_path, alert: "Please confirm your email address to continue."
+    end
+  end
+
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
     devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name])
