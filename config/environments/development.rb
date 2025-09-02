8a57c5cd2a8e19559e09bc57c533bd6f00468eef
IBUNHABIBU <ibunhabib@gmail.com>
2025-09-02 03:23:54 +0300

    Add latter opener gem

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -5,7 +5,7 @@ Rails.application.configure do
 
     # config/environments/development.rb
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
-  config.action_mailer.delivery_method = :smtp
+  # config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
 
 # For testing in development, you can use letter_opener gem
