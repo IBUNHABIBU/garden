a38746f42736d1f58bc1a85124b643488b853586
IBUNHABIBU <ibunhabib@gmail.com>
2025-09-28 17:03:23 +0300

    Working slider without stimulus

--- app/javascript/controllers/slider_controller.js
+++ app/javascript/controllers/slider_controller.js
@@ -1,76 +1,36 @@
-// import { Controller } from "@hotwired/stimulus";
+import { Controller } from "@hotwired/stimulus"
 
-// export default class extends Controller {
-//   static targets = ["dot", "slide"];
-//   static values = { index: Number }
+export default class extends Controller {
+  static targets = ["slides", "dot"]
+  static values = { index: Number }
 
-//   connect() {
-//     this.indexValue = 0;
-//     this.showSlide();
-//     this.startAutoSlide();
-    
-//     // Add event listeners for navigation arrows
-//     this.element.querySelector('.slider-prev').addEventListener('click', () => {
-//       this.prevSlide();
-//     });
-    
-//     this.element.querySelector('.slider-next').addEventListener('click', () => {
-//       this.nextSlide();
-//     });
-//   }
+  connect() {
+    this.indexValue = 0
+    this.update()
+  }
 
-//   disconnect() {
-//     this.stopAutoSlide();
-//   }
+  next() {
+    this.indexValue = (this.indexValue + 1) % this.slidesTarget.children.length
+    this.update()
+  }
 
-//   startAutoSlide() {
-//     this.interval = setInterval(() => {
-//       this.nextSlide();
-//     }, 5000);
-//   }
+  prev() {
+    this.indexValue = (this.indexValue - 1 + this.slidesTarget.children.length) % this.slidesTarget.children.length
+    this.update()
+  }
 
-//   stopAutoSlide() {
-//     if (this.interval) {
-//       clearInterval(this.interval);
-//     }
-//   }
+  goToSlide(event) {
+    this.indexValue = parseInt(event.currentTarget.dataset.index, 10)
+    this.update()
+  }
 
-//   showSlide(index = this.indexValue) {
-//     const slides = this.element.querySelectorAll('.slides__bg');
-    
-//     slides.forEach(slide => {
-//       slide.classList.add('hidden');
-//     });
-    
-//     if (slides[index]) {
-//       slides[index].classList.remove('hidden');
-//     }
-    
-//     this.dotTargets.forEach((dot, i) => {
-//       const isActive = i === index;
-//       dot.classList.toggle('bg-opacity-100', isActive);
-//       dot.classList.toggle('bg-opacity-50', !isActive);
-//     });
-    
-//     this.indexValue = index;
-//   }
+  update() {
+    const offset = -this.indexValue * 100
+    this.slidesTarget.style.transform = `translateX(${offset}%)`
 
-//   nextSlide() {
-//     const slides = this.element.querySelectorAll('.slides__bg');
-//     this.indexValue = (this.indexValue + 1) % slides.length;
-//     this.showSlide();
-//   }
-
-//   prevSlide() {
-//     const slides = this.element.querySelectorAll('.slides__bg');
-//     this.indexValue = (this.indexValue - 1 + slides.length) % slides.length;
-//     this.showSlide();
-//   }
-
-//   goToSlide(event) {
-//     const index = parseInt(event.currentTarget.dataset.index);
-//     this.stopAutoSlide();
-//     this.showSlide(index);
-//     this.startAutoSlide();
-//   }
-// }
+    this.dotTargets.forEach((dot, i) => {
+      dot.classList.toggle("bg-opacity-100", i === this.indexValue)
+      dot.classList.toggle("bg-opacity-50", i !== this.indexValue)
+    })
+  }
+}
