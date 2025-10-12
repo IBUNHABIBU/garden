ed700a299155a97aa7bdace96edbd5c7c0fe5ee8
IBUNHABIBU <ibunhabib@gmail.com>
2025-10-12 18:49:06 +0300

    Add eager loading

--- app/controllers/safaris_controller.rb
+++ app/controllers/safaris_controller.rb
@@ -3,7 +3,7 @@ class SafarisController < ApplicationController
 
   # GET /safaris or /safaris.json
   def index
-    @safaris = Safari.with_attached_images.all
+    @safaris = Safari.with_attached_main_image.with_attached_gallery_images.all
   end
 
   # GET /safaris/1 or /safaris/1.json
