28d131c2b60523bec31f62b40281b8d0997ff9a9
IBUNHABIBU <ibunhabib@gmail.com>
2026-03-01 21:03:27 +0300

    Add user to bookings

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_10_21_084752) do
+ActiveRecord::Schema[8.0].define(version: 2026_03_01_180052) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -64,6 +64,8 @@ ActiveRecord::Schema[8.0].define(version: 2025_10_21_084752) do
     t.string "status"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_bookings_on_user_id"
   end
 
   create_table "destinations", force: :cascade do |t|
@@ -212,4 +214,5 @@ ActiveRecord::Schema[8.0].define(version: 2025_10_21_084752) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "bookings", "users"
 end
