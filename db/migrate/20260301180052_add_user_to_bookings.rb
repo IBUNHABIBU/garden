ee6a94adf6e81c30b0046d341b1ef9af96fe64c4
IBUNHABIBU <ibunhabib@gmail.com>
2026-03-02 22:36:20 +0300

    Add user to bookings

--- db/migrate/20260301180052_add_user_to_bookings.rb
+++ db/migrate/20260301180052_add_user_to_bookings.rb
@@ -1,5 +1,5 @@
 class AddUserToBookings < ActiveRecord::Migration[8.0]
   def change
-    add_reference :bookings, :user, null: false, foreign_key: true
+    add_reference :bookings, :user, null: true, foreign_key: true
   end
 end
