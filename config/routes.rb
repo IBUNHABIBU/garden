ef5f3a75da3928d342433081ea9f5b10fd30d308
IBUNHABIBU <ibunhabib@gmail.com>
2026-03-10 23:09:37 +0300

    Add review link

--- config/routes.rb
+++ config/routes.rb
@@ -34,7 +34,7 @@ get 'gallery', to: 'heros#gallery'
   resources :trekkings, path: 'treks' 
   resources :safaris, path: 'wildlife-safaris'
   resources :testimonials, path: 'reviews'
-  get "add_review", to: "reviews#new"
+  get "add_review", to: "testimonials#new"
 
   resources :travel_tours do
     member do
